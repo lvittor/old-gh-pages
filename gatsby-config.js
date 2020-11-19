fcb818d0df2fba817b9d37de2d37fb773d7957ac
lvittor <lvittor@itba.edu.ar>
2020-11-19 17:06:38 -0300

    viti icon in tab addded

--- gatsby-config.js
+++ gatsby-config.js
@@ -150,7 +150,7 @@ module.exports = {
             background_color: `#663399`,
             theme_color: `#663399`,
             display: `minimal-ui`,
-            icon: `src/images/oscar-icon.png`, // This path is relative to the root of the site.
+            icon: `src/images/img-viti.png`, // This path is relative to the root of the site.
         },
     },
     {
