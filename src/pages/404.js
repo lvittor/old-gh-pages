0c2654e85fa3d1a9bf72b68e4c3d334c63ae87e3
lvittor <lvittor@itba.edu.ar>
2021-02-21 20:27:45 -0300

    remove comments

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,42 +1,17 @@
 import React from "react";
-import ContestantHead from "components/Bachelor/ContestantHead"
 import Label from "components/_ui/Label/Label";
 import Layout from "components/Layout/Layout";
 import MaxWidth from "components/_ui/MaxWidth/MaxWidth";
 import Link from "components/_ui/Link/Link";
-import Spinner from "components/_ui/Spinner/Spinner";
 import "./404.scss";
 
 const NotFoundPage = () => (
-    <Layout showNavigation showSocialCol showFooter>
+    <Layout showNavigation showSocialCol showFooter> {/*se podria eliminar el showFooter porque se ve mal*/}
         <MaxWidth className="NotFound">
-            {/*<Spinner
-                className="NotFound__spinner__container"
-                pet="vitipc"
-                size="xl"
-            />*/}
-            {/*<span className="emoji" role="img" aria-label="Emoji Joker">🃏</span>*/}
             <h1 className="NotFound__header">
                 ooops, 404.
             </h1>
             <div className="NotFound__recent">
-                {/*<div>
-                    <Label>
-                        Check out my recent project
-                    </Label>
-                    <Link to="/2020-bachelor" className="NotFound__recent__item NotFound__recent__item--project">
-                        <ContestantHead name="Deandra" />
-                        <div>
-                            <h3>
-                                Bachelor 2020 Contestant Guide
-                            </h3>
-                            <p>
-                                Updating weekly with episode information and improved components. Wip!
-                            </p>
-                        </div>
-
-                    </Link>
-                </div>*/}
                 <div>
                     <Label>
                         último post
