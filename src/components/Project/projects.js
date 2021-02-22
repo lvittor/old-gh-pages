b408af608762a06e25a4cb1af8d783e8f3cdb7cc
lvittor <lvittor@itba.edu.ar>
2021-02-22 02:31:20 -0300

    remove comments

--- src/components/Project/projects.js
+++ src/components/Project/projects.js
@@ -1,26 +1,6 @@
 import React from "react";
 
 const projects = {
-    // currents: {
-    //     title: "Currents Dashboard",
-    //     projectType: "dashboard",
-    //     description: <div>
-    //         A look into the Currents Attention analytics dashboard built at Parse.ly.
-    //     </div>,
-    //     readMore: "./currents",
-    //     "tech": [
-    //         "react",
-    //         "redux",
-    //         "d3",
-    //         "mixpanel",
-    //         "figma"
-    //     ],
-    //     hasImageBorder: true,
-    //     imgType: "mp4",
-    //     "img_src": [
-    //         "project-currents-01"
-    //     ],
-    // },
     candyCrush: {
         title: "Candy Crush 🎮",
         projectType: "game development",
@@ -73,66 +53,6 @@ const projects = {
             "project-weather-status-01"
         ],
     },
-    /*prist: {
-        title: "Prist | A Gatsby & Prismic Starter",
-        projectType: "web",
-        description: <div>
-            A light-themed starter powered
-            by <a href="https://www.gatsbyjs.org/" rel="noopener noreferrer" target="_blank">Gatsby v2</a> and <a href="https://www.gatsbyjs.org/" rel="noopener noreferrer" target="_blank">Prismic</a> to showcase portfolios and blogs.
-            <br />
-            <br />
-            <a href="https://www.gatsbyjs.org/starters/margueriteroth/gatsby-prismic-starter-prist/" rel="noopener noreferrer" target="_blank">Gatsby starter doc</a>
-        </div>,
-        demo: "https://prist.marguerite.io/",
-        github: "https://github.com/margueriteroth/gatsby-prismic-starter-prist",
-        "tech": [
-            "gatsby",
-            "graphql",
-            "cms",
-            "react",
-            "figma",
-            "netlify",
-        ],
-        hasImageBorder: true,
-        "img_src": [
-            "project-prist-01"
-        ],
-    },
-    /*retention: {
-        title: "Parse.ly Analytics Data Retention Upsells",
-        projectType: "web",
-        description: <div>
-            Working through adding more obvious data retention limit communication in the analytics dashboard and allowing users to upgrade.
-        </div>,
-        readMore: "/data-retention",
-        "tech": [
-            "angular",
-            "less",
-            "figma",
-        ],
-        hasImageBorder: true,
-        "img_src": [
-            "project-data-retention-01"
-        ],
-    },*/
-    /*albondiga: {
-        title: "Albondiga | App for requesting some dinner 🍝",
-        projectType: "web",
-        description: "I developed this project as a birthday gift for my partner — a lover of spaghetti and meatballs. The recipient can pick the ingredients making up a spaghetti and meatballs dinner: pasta type, sauce, drinks, etc.",
-        demo: "https://margueriteroth.github.io/albondiga/",
-        github: "https://github.com/margueriteroth/albondiga",
-        "tech": [
-            "react",
-            "firebase",
-            "scss",
-            "bem",
-            "node"
-        ],
-        "img_src": [
-            "project-albondiga-01"
-        ],
-    },
-    */
 };
 
 export default projects;
